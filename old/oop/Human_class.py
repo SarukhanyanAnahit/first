b8e124fbbebc7af62c85b7745ee9d86516f66067
Anahit <sarukhanyananahit03@gmail.com>
2025-07-07 13:40:53 +0400

    Human

--- old/oop/Human_class.py
+++ old/oop/Human_class.py
@@ -1,17 +1,33 @@
-class Human():
+from abc import ABC, abstractmethod
+
+class Human(ABC):
     def __init__(self, n, s, a):
         self.__name = n
         self.__surname = s
         self.__age = a
 
+    def set_name(self, name):
+        self.__name = name
+
     def get_name(self):
         return self.__name
+
+    def set_surname(self, surname):
+        self.__surname = surname
 
     def get_surname(self):
         return self.__surname
 
+    def set_age(self, age):
+        self.__age = age
+
     def get_age(self):
         return self.__age
+
+    @abstractmethod
+    def __str__(self):
+        pass
+
 
 class Employee(Human):
     def __init__(self, name, surname, age):
@@ -24,6 +40,10 @@ class Employee(Human):
     def get_manager(self):
         return self.__manager
 
+    def __str__(self):
+        return f"employee: {self.get_name()} {self.get_surname()}, age: {self.get_age()}"
+
+
 class Manager(Employee):
     def __init__(self, name, surname, age):
         super().__init__(name, surname, age)
@@ -31,7 +51,7 @@ class Manager(Employee):
 
     def hire(self, employee):
         if employee.get_manager() is not None:
-            print(f"{employee.get_name()} is already hired by another manager.")
+            print(f"{employee.get_name()} is already hired")
             return
 
         if len(self.__employees) >= 10:
@@ -50,16 +70,21 @@ class Manager(Employee):
         else:
             print(f"{employee.get_name()}, {employee.get_surname()} is not an employee of {self.get_name()}")
 
+    def __str__(self):
+        return f"manager: {self.get_name()} {self.get_surname()}, age: {self.get_age()}, employees: {len(self.__employees)}"
+
 first_manager = Manager("Vardanyan", "Ani", 40)
 second_manager = Manager("Hakobyan", "Arshak", 38)
 
 employee1 = Employee("Janna", "Hakobyan", 25)
 employee2 = Employee("Larisa", "Hovhannisyan", 28)
 employee3 = Employee("Armine", "Davtyan", 35)
-
 first_manager.hire(employee1)
 second_manager.hire(employee2)
 second_manager.hire(employee1)
 first_manager.fire(employee1)
 first_manager.fire(employee2)
 second_manager.hire(employee1)
+
+print(employee3)
+print(second_manager)
